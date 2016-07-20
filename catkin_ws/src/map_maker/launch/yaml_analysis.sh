229dbebd4d9a7b6234e56a7117c99d7b3564601c
jackstrang <jack.s.strang@gmail.com>
2016-07-20 14:22:45 -0400

    3 building types displayed in equal proportion

--- catkin_ws/src/map_maker/launch/yaml_analysis.sh
+++ catkin_ws/src/map_maker/launch/yaml_analysis.sh
@@ -11,8 +11,28 @@ cat $map_file
 str='map_pre_dict:'
 
 line=`grep "$str" $map_file`
+#IFS=':'; line2=($line); unset IFS;
 
-echo $line
+line2=`echo $line |  tr : ' '`
+line3=`echo $line2 | tr , ' '`
+#echo $line2
 
-num=`echo $line | sed -e 's/\(.\)/\1\n/g' | grep 1 | wc -'[]'`
-echo num
+#num=`echo $line | sed -e 's/\(.\)/\1\n/g' | grep 1 | wc -'[]'`
+#num=grep word1 $line | wc -'[]'
+#big_num=`grep -o '[]' $map_file | wx -l`
+
+#echo $big_num
+
+
+setopt shwordsplit
+
+count=0
+for thing in $line3; do
+	echo $thing
+	if [ $thing = '[]' ]; then
+		count=$(($count+1))
+	fi
+done
+
+
+echo $count
